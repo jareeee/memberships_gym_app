b68ed01eb143fa4f6ba4db0d32fe125d3a84afdd
jareeee <fajarmulyos@gmail.com>
2025-07-08 00:08:27 +0700

    Added Feat: Implement Google OAuth2 authentication with Devise

--- Gemfile
+++ Gemfile
@@ -52,6 +52,9 @@ gem 'devise'
 # gem 'omniauth-google-oauth2'
 gem 'omniauth-google-oauth2'
 
+gem 'omniauth-rails_csrf_protection'
+
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
+# frozen_string_literal: true
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  devise :omniauthable, omniauth_providers: [:google_oauth2]
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+    user ||= User.create(
+      full_name: data['name'],
+      email: data['email'],
+      password: Devise.friendly_token[0..20]
+    )
+
+    user
+  end
 end
