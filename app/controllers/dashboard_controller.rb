11be6430d83deb6bdbba030a84fc89d797740c7d
jareeee <fajarmulyos@gmail.com>
2025-07-14 23:22:07 +0700

    Add recent check-ins section to dashboard view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,6 @@ class DashboardController < ApplicationController
 
   def show
     @membership = current_user.memberships.active.first
-    # @recent_check_ins = current_user.check_ins.order(check_in_timestamp: :desc).limit(5)
+    @recent_check_ins = current_user.check_ins.includes(:gym_location).order(check_in_timestamp: :desc).limit(5)
   end
 end
